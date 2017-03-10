4a2693299f74d3dd59e3ebfcc8ee5f1443f2c977
Teigen Leonard <teigenl@gmail.com>
2017-03-10 11:42:45 -0600

    add infinite silliness

--- client.js
+++ client.js
@@ -6,10 +6,21 @@ $(document).ready(function(){
 function appendDOM(){
   $(".container").append("<div class='box'></div>");
 
-  $("container").append("<p>first</p>");
-  $("container").append("<p id='second'>Second</p>");
-  $("container").append("<p>third</p>");
-  $("#second").fadeToggle(5000);
+  $(".container").append("<p>first</p>");
+  $(".container").append("<p id='second'>Second</p>");
+  $(".container").append("<p>third</p>");
+  // $("#second").fadeToggle(5000);
+    // $("#second").fadeToggle(1000).fadeToggle(10000);
+    // $("#second").slidedown(5000); //slide down to reveal so slide down only works on hidden elements
+endlessLoop ($("#second"));
+endlessLoop (".container").children().first();
+endlessLoop (".container>").chilren().last();
+}
+
+function endlessLoop(element){
+  element.slidToggle(3000, function(){
+      endlessLoop(element);
+  });
 }
 
 function addEventListeners(){
@@ -22,6 +33,7 @@ function addEventListeners(){
   // });
   $(".container").on("mouseover" , ".box" , function(){
     $(this).toggleClass("highlight");
+    console.log("here!");
   });
 
   $(".container").on("mouseout" , ".box" , function(){
